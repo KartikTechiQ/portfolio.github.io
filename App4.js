5811c52d833bf0b6c518a13959a3928a2693b3de
Kartikeya <157104030+KartikTechiQ@users.noreply.github.com>
2024-03-01 12:26:12 +0530

    my portfolio website
    attached with resume

---
+++ App4.js
@@ -0,0 +1,14 @@
+
+// const tablinks =document.getElementsByClassName('tab-links');
+// const tabcontents  =document.getElementsByClassName('tab-contents');
+
+// function opentab(tabname){
+//     for(tablink of tablinks){
+//         tablinks.classlist.remove("active-link")
+//     }
+// }
+//     for(tabcontent of tabcontents){
+//         tabcontent.classlist.remove("active-tab")
+//     }
+
+
